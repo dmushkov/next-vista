36cd81b069c4f02d4a1c4649b8961857f59ced8c
dian mushkov <dian.mushkov@gmail.com>
2024-12-20 11:23:52 +0200

    Initial

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker'
 import bcrypt from 'bcrypt'
 
-import prisma from '@/lib/prisma'
+import prisma from '../lib/prisma'
 
 import { revenue, users } from './data'
 
